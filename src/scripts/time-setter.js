41c32c56bb360437289651e6ff53ee74570433f5
Navi474 <v0735548277@gmail.com>
2025-04-11 23:33:31 +0300

    finish of time-setter

--- src/scripts/time-setter.js
+++ src/scripts/time-setter.js
@@ -1,5 +1,6 @@
 const main_func = () => {
     const input_of_num = document.getElementById("input_of_num");
+    const fourth_half_of_time = document.getElementById("4th_half_of_time");
     const first_half_of_time = document.getElementById("first_half_of_time");
     const second_half_of_time = document.getElementById("second_half_of_time");
     const third_half_of_time = document.getElementById("third_half_of_time");
@@ -8,12 +9,14 @@ const main_func = () => {
 
     btn1.addEventListener("click", () => {
         const num = input_of_num.value
-        const output1 = String(Math.floor(num / 3600))
+        const output4 = String(Math.floor(num / 86400) % 3600)
+        const output1 = String(Math.floor(num / 3600) % 60)
         let output2 = String(Math.floor(num / 60) % 60)
         let output3 = String(num % 60)
         first_half_of_time.innerText = output1
         second_half_of_time.innerText = output2;
         third_half_of_time.innerText = output3;
+        fourth_half_of_time.innerText = output4
     })
 }
 main_func();
